e844f80f063e3f5be8d1200ded20e4e1dd7d7386
gerodrus <gerodrus@gmail.com>
2022-05-28 13:07:03 +0300

    Create a new project

---
+++ Source/UnrealUtils/UnrealUtils.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealUtils.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealUtils, "UnrealUtils" );
---
+++ Source/UnrealUtils/UnrealUtilsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealUtilsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALUTILS_API AUnrealUtilsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
